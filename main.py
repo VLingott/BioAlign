a928a4d44fc0c6b852a42fee973aaebfe3a084d0
Vitus Lingott <mastergreeny@duck.com>
2025-03-28 18:53:17 +0100

    fix(v1.1.0): Correct minor typo in a print statement in main.py

--- main.py
+++ main.py
@@ -616,7 +616,7 @@ def main():
             file.write(new_hash)
         print("Computing DNA sequence alignment...", end='')
         prepare_seq(sequences, "sequences.aln")
-        print("\rComputed DNA sequence alignment.  \n")
+        print("\rComputed DNA sequence alignment.   \n")
     else:
         print("Reusing unchanged DNA alignment file.\n")
 
